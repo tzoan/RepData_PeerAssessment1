d409b95630fdcae72c6444f510d1ffd5ddd1474c
tzoan <antzogani@gmail.com>
2021-03-08 18:44:08 +0200

    initial commit

--- activity.R
+++ activity.R
@@ -1,24 +1,21 @@
-#Reproducible Research: Assignment 1
 
-##Download file and unzip data
-```{r}
+##download file and unzip data
 if(!file.exists("./reproducibleresearch")){dir.create("./reproducibleresearch")}
 fileurl <- "https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2Factivity.zip"
 download.file(fileurl, destfile = "./reproducibleresearch/repdata_data_activity.zip")
 unzip(zipfile = "./reproducibleresearch/repdata_data_activity.zip", exdir = "./reproducibleresearch")
-```
 
-##Reading and exploring activity data
-```{r}
+
+#reading and exploring activity data
 library(lubridate)
 activity<-read.csv("./reproducibleresearch/activity.csv",header=TRUE)
-str(activity)
+str(activity)## 17568 observations of 3 variables:steps, data, interval
+head(activity,n=3)
 activity$date <- ymd(activity$date)
 activity$weekend <- as.factor(ifelse(weekdays(activity$date)=="Saturday" | weekdays(activity$date)=="Sunday","weekend","weekday"))
 activity$dayofweek <- as.factor(weekdays(activity$date))
-```
-##Total number of steps taken each day
-```{r}
+
+##total number of steps
 library(dplyr)
 library(ggplot2)
 stepsbyday<- activity %>%  # Group by day and summarize total steps across the board
@@ -26,110 +23,67 @@ stepsbyday<- activity %>%  # Group by day and summarize total steps across the b
         group_by(date) %>%
         summarize(Stepsperday= sum(steps, na.rm = TRUE))
 #Histogram of the total number of steps taken each day
+png("plot1.png",width=480, height=480)
 qplot(Stepsperday,data=stepsbyday,na.rm=TRUE,binwidth=500,xlab='Total steps per day', ylab='Frequency using binwith 500',main = 'Histogram of the total number of steps taken each day')
-```
-## Mean and median steps taken each day
-```{r}
-summary(stepsbyday)
-Mean=as.integer(mean(stepsbyday$Stepsperday))
-Median=as.integer(median(stepsbyday$Stepsperday))
-```
-The mean steps taken each day is **`r Mean`** and the median **`r Median`**.
+dev.off
 
-##Time series plot of the average number of steps taken
-```{r}
+# Mean and median steps taken each day
+summary(stepsbyday)
+Mean=mean(stepsbyday$Stepsperday)
+Median=as.integer(median(stepsbyday$Stepsperday))
+
+#time series plot of the average number of steps taken
 interval_average <- activity %>% 
         group_by(interval) %>% 
         summarise(average = mean(steps,na.rm = TRUE))
+interval_average
 ggplot(interval_average,aes(interval,average))+geom_line()+labs(x="5-minute interval",y="Average steps taken across all days")
-```
 
 ##The 5-minute interval that, on average, contains the maximum number of steps
-```{r}
-Max=interval_average[which.max(interval_average$average),]
-```
+interval_average[which.max(interval_average$average),]
 
 ##Imputing data
-
-Subset dataset where there are no Missing Values
-
-```{r}
 activity_no_NA <- activity[which(!is.na(activity$steps)),]
-```
 
-Average number of steps taken without Missing Values
-
-```{r}
+##average number of steps taken without NA
 interval_average_no_NA <- activity_no_NA %>% 
         group_by(interval) %>% 
         summarise(average = mean(steps,na.rm = TRUE))
-```
+interval_average_no_NA
 
-Convert the average to integer.
-
-```{r}
+# convert the average to integer
 interval_average_no_NA$average <- as.integer(interval_average_no_NA$average)
-```
 
-Subset dataset where steps have Missing Values.
-
-```{r}
+#subset dataset where steps have NAs
 activity_na <- activity[which(is.na(activity$steps)),]
-```
 
-
-```{r}
-dataset.na=nrow(activity_na)
-```
-
-The number of Missing Values in the dataset:**`r dataset.na`**
-
-Fill NAs with average steps based on interval
-
-```{r}
+# fill NAs with average steps based on interval
 activity_na$steps <- ifelse(activity_na$interval==interval_average_no_NA$interval,interval_average_no_NA$average)
-```
 
-Row bind the datasets that do not have Missing Values and the dataset where Mising Values are replaced with mean values.
-
-```{r}
+# row bind the datasets that do not have NAs and the dataset where NAs are replaced with
 activity_impute <- rbind(activity_no_NA,activity_na)
 str(activity_impute)
-```
 
-##Steps per day after imputing data
-
-```{r}
+##Steps per day
 stepsbyday_impute<- activity_impute %>%  # Group by day and summarize total steps across the board
         select(date,steps)%>%
         group_by(date) %>%
         summarize(Stepsperday= sum(steps, na.rm = TRUE))
-```
+stepsbyday_impute
 
-##Histogram of the total number of steps taken each day after impute data
-
-```{r}
+#Histogram of the total number of steps taken each day after impute data
+png("plot1.png",width=480, height=480)
 qplot(Stepsperday,data=stepsbyday_impute,na.rm=TRUE,binwidth=500,xlab='Total steps per day', ylab='Frequency using binwith 500',main = 'Histogram of the total number of steps taken each day')
-```
+dev.off
 
-##Mean and Median
-```{r}
+#mean and median
 summary(stepsbyday_impute)
-Mean_impute=as.integer(mean(stepsbyday_impute$Stepsperday))
-Median_impute=as.integer(median(stepsbyday_impute$Stepsperday))
-```
+mean(stepsbyday_impute$Stepsperday)
+median(stepsbyday_impute$Stepsperday)
 
-The mean steps taken each day is **`r Mean_impute`** and the median **`r Median_impute`**.
-
-##Panel plot in activity patterns between weekdays and weekends
-
-```{r}
+#Panel plot
 meansteps <- activity_impute %>% group_by(interval,weekend) %>% summarise(average = mean(steps))
 ggplot(meansteps,aes(interval,average,color=weekend))+geom_line()+facet_grid(weekend~.) +xlab("Interval") + ylab("Mean of Steps") +ggtitle("Comparison of Average Number of Steps in Each Interval")
-```
-
-
-
 
 
 
